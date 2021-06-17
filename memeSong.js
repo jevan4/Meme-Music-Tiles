2c2d34f9da1957ada55d94e79860d6fa6d5e086f
jevan4 <83592994+jevan4@users.noreply.github.com>
2021-06-17 18:43:33 -0400

    Create memeSong.js

---
+++ memeSong.js
@@ -0,0 +1,10 @@
+//this is a wrapper for a specific song. it contains the asset path to the mp3 itself along with meta data such as bpm and name
+class MemeSong {
+  
+  constructor(songName,songPath,songBpm){
+    this.name=songName;
+    this.path=songPath;
+    this.bpm=songBpm;
+  }
+  
+}
